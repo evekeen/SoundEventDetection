c551c280d25f672fee2c8692e7bf35cf23a13df3
Alexander Ivkin <a@ivkin.dev>
2024-06-23 15:34:21 +0200

    updated script

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@ cd /acetrace
 git clone https://github.com/evekeen/SoundEventDetection git
 cd /acetrace/git
 
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set default.region $AWS_DEFAULT_REGION
-
+aws configure
 aws s3 cp s3://acetrace/wav/ /acetrace/git/data/wav --recursive
 aws s3 cp s3://acetrace/wav_metadata/ /acetrace/git/data/wav_metadata --recursive
