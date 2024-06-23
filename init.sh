0b8be60d8fa242c62df99e484cae99d1c2f532e0
Alexander Ivkin <a@ivkin.dev>
2024-06-23 15:03:59 +0200

    ready for training on vast.ai

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
 cd /acetrace
 git clone https://github.com/evekeen/SoundEventDetection git
 cd /acetrace/git
-python main.py
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
+
+aws s3 cp s3://acetrace/wav/ /acetrace/git/data/wav --recursive
+aws s3 cp s3://acetrace/wav_metadata/ /acetrace/git/data/wav_metadata --recursive
