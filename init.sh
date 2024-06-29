cc8c0baeb5a7945b84940e444e405353766fdba3
Alexander Ivkin <a@ivkin.dev>
2024-06-29 17:42:44 +0200

    copy files to data

--- init.sh
+++ init.sh
@@ -3,5 +3,7 @@ git clone https://github.com/evekeen/SoundEventDetection git
 cd /acetrace/git
 
 aws configure
-aws s3 cp s3://acetrace/wav/ /acetrace/git/data/wav --recursive
-aws s3 cp s3://acetrace/wav_metadata/ /acetrace/git/data/wav_metadata --recursive
+aws s3 cp s3://acetrace/wav_2024_06_29/ /acetrace/git/data/wav --recursive
+
+cp /acetrace/git/data/wav/*.wav /acetrace/git/data/Tau_sound_events_2019/raw/foa_eval/
+cp /acetrace/git/data/wav/*.csv /acetrace/git/data/Tau_sound_events_2019/raw/metadata_eval/
