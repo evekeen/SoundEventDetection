d53f169d93cf2d285a8adea7033374cd38d59b1a
Alexander Ivkin <a@ivkin.dev>
2024-07-01 14:26:34 +0200

    removed more non SED code

--- models/DcaseNet.py
+++ models/DcaseNet.py
@@ -8,18 +8,10 @@ import torch.nn.functional as F
 from models.model_utilities import ConvBlock, init_gru, init_layer, interpolate
 
 def get_DcaseNet_v3(**args):
-    return DcaseNet_v3(block = SEBasicBlock, **args)
+    return DcaseNet_v3(**args)
 
 class DcaseNet_v3(nn.Module):
-    def __init__(self,
-        block,
-        filts_ASC = 256,
-        blocks_ASC = 3,
-        strides_ASC = 2,
-        code_ASC = 128,
-        pool_type='avg',
-        pool_size=(2,2)):
-        
+    def __init__(self, pool_type='avg', pool_size=(2,2)):
         super().__init__()
 
         #####
@@ -41,34 +33,6 @@ class DcaseNet_v3(nn.Module):
         self.event_fc = nn.Linear(256, 14, bias=True)
 
         #####
-        # ASC
-        #####
-        self.inplane = 256
-        self.layer_ASC = self._make_layer(
-            block = block,
-            planes = 384,
-            blocks = blocks_ASC,
-            stride=strides_ASC,
-            reduction=16
-        )
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.code_ASC = nn.Linear(384, code_ASC)
-        self.fc_ASC = nn.Linear(code_ASC, 10)
-
-        #####
-        # TAG
-        #####
-        self.layer_TAG = nn.Sequential(
-            nn.Linear(512, 1024),
-            nn.ReLU(),
-            nn.Dropout(p=0.2),
-            nn.Linear(1024, 1024),
-            nn.ReLU(),
-            nn.Dropout(p=0.1),
-            nn.Linear(1024, 80)
-        )
-
-        #####
         # Initialize
         #####
         self.init_weights()
@@ -78,15 +42,6 @@ class DcaseNet_v3(nn.Module):
         init_gru(self.gru_1)
         init_gru(self.gru_2)
         init_layer(self.event_fc)
-
-    def _make_layer(self, block, planes, blocks, stride, reduction):
-        strides = [stride] + [1] * (blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.inplane, planes, stride, reduction))
-            self.inplane = planes
-
-        return nn.Sequential(*layers)
 
     def forward(self, x):
         d_return = {}   # dictionary to return
@@ -118,60 +73,6 @@ class DcaseNet_v3(nn.Module):
         out_SED = out_SED.repeat_interleave(repeats=8, dim=1)
         return out_SED
 
-#####
-# ASC
-#####
-def conv3x3(in_planes, out_planes, stride=1):
-    #changed from 1d to 2d
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
-
-class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
-            nn.Sigmoid()
-        )
-
-    def forward(self, x):
-        b, c, _, _ = x.size()
-        y = self.avg_pool(x).view(b, c)
-        y = self.fc(y).view(b, c, 1, 1)
-        return x * y.expand_as(x)
-
-class SEBasicBlock(nn.Module):
-    def __init__(self, inplanes, planes, stride=1, reduction=16):
-        super(SEBasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.se = SELayer(planes, reduction)
-        if inplanes != planes:
-            self.downsample = nn.Sequential(nn.Conv2d(inplanes, planes, kernel_size=1, stride=stride, bias=False),
-                                            nn.BatchNorm2d(planes))
-        else:
-            self.downsample = lambda x: x
-        self.stride = stride
-
-    def forward(self, x):
-        residual = self.downsample(x)
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.se(out)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
 
 if __name__ == '__main__':
     pass
