740fa059e15abe21fcae2fb3f01a12cdddfc11fd
Alexander Ivkin <a@ivkin.dev>
2024-07-01 14:53:13 +0200

    refactoring

--- detect_loud_sound.py
+++ detect_loud_sound.py
@@ -51,39 +51,12 @@ def find_loud_intervals(file_path, output, visualise=False):
     if start_time is not None:
         loud_intervals.append((start_time, 1.0))
         
-    merged_intervals = []
-    merged_energies = []
-    merged_interval_energy = 0
-    i = 0
-    current_start_time = 0
-    while i < len(loud_intervals) - 1:
-        start_time, end_time = loud_intervals[i]
-        current_start_time = start_time if current_start_time == 0 else current_start_time
-        
-        next_start_time, next_end_time = loud_intervals[i+1]
-        if next_start_time - end_time <= 5000.0 / sr:
-            print('Adding end time to the next merge:', next_end_time)
-            end_time = next_end_time 
-        else:
-            print('Merging interval:', current_start_time, end_time)
-            merged_intervals.append((current_start_time, end_time))
-            merged_interval_energy = max(merged_interval_energy, energies[i])
-            merged_energies.append(merged_interval_energy)
-            merged_interval_energy = 0
-            current_start_time = 0
-        i += 1
-    if current_start_time != 0:
-        merged_intervals.append((current_start_time, loud_intervals[-1][1]))
-        merged_interval_energy = max(merged_interval_energy, energies[-1])
-        merged_energies.append(merged_interval_energy)
-    if loud_intervals:
-        merged_intervals.append(loud_intervals[-1])
-        merged_energies.append(energies[-1] if energies else 0)  
-    loud_intervals = merged_intervals
+    
+    loud_intervals, load_energies = merge_intervals(loud_intervals, energies)
     if not loud_intervals:
         print(f"NOT_DETECTED: {file_path}")
         return None
-    loudest_interval_index = np.argmax(merged_energies)
+    loudest_interval_index = np.argmax(load_energies)
     loudest_interval = loud_intervals[loudest_interval_index]
     print("Loudest Interval:", loudest_interval)
     
@@ -113,6 +86,38 @@ def find_loud_intervals(file_path, output, visualise=False):
         plt.show()
 
     return loudest_interval
+
+def merge_intervals(intervals, energies):
+    merged_intervals = []
+    merged_energies = []
+    merged_interval_energy = 0
+    i = 0
+    current_start_time = 0
+    while i < len(intervals) - 1:
+        start_time, end_time = intervals[i]
+        current_start_time = start_time if current_start_time == 0 else current_start_time
+        
+        next_start_time, next_end_time = intervals[i+1]
+        if next_start_time - end_time <= 5000.0 / sr:
+            print('Adding end time to the next merge:', next_end_time)
+            end_time = next_end_time 
+        else:
+            print('Merging interval:', current_start_time, end_time)
+            merged_intervals.append((current_start_time, end_time))
+            merged_interval_energy = max(merged_interval_energy, energies[i])
+            merged_energies.append(merged_interval_energy)
+            merged_interval_energy = 0
+            current_start_time = 0
+        i += 1
+    if current_start_time != 0:
+        merged_intervals.append((current_start_time, intervals[-1][1]))
+        merged_interval_energy = max(merged_interval_energy, energies[-1])
+        merged_energies.append(merged_interval_energy)
+    if intervals:
+        merged_intervals.append(intervals[-1])
+        merged_energies.append(energies[-1] if energies else 0)  
+        
+    return merged_intervals, merged_energies
 
 def export_interval_wav(file_path, interval, output_file):
     y, sr = librosa.load(file_path, sr=None, duration=2)
