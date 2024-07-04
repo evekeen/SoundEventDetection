afa407af7549b37211a14adb535d60b222999a11
Alexander Ivkin <a@ivkin.dev>
2024-07-04 17:24:09 +0200

    only 2 layers

--- models/DcaseNet.py
+++ models/DcaseNet.py
@@ -21,11 +21,11 @@ class DcaseNet_v3(nn.Module):
         
         self.conv_block1 = ConvBlock(in_channels=2, out_channels=64)
         self.conv_block2 = ConvBlock(in_channels=64, out_channels=128)
-        self.conv_block3 = ConvBlock(in_channels=128, out_channels=256)
-        self.conv_block4_1 = ConvBlock(in_channels=256, out_channels=256)
-        self.conv_block4_2 = ConvBlock(in_channels=256, out_channels=256)
+        self.conv_block3 = ConvBlock(in_channels=128, out_channels=128)
+        # self.conv_block4_1 = ConvBlock(in_channels=256, out_channels=256)
+        # self.conv_block4_2 = ConvBlock(in_channels=256, out_channels=256)
 
-        self.gru_2 = nn.GRU(input_size=256, hidden_size=128, num_layers=1, batch_first=True, bidirectional=True)
+        self.gru_2 = nn.GRU(input_size=128, hidden_size=128, num_layers=1, batch_first=True, bidirectional=True)
         self.event_fc = nn.Linear(256, class_num, bias=True)
 
         #####
@@ -45,7 +45,7 @@ class DcaseNet_v3(nn.Module):
         x = self.conv_block1(x)
         x = self.conv_block2(x)
         x = self.conv_block3(x)
-        x = self.conv_block4_1(x)   #common branch
+        # x = self.conv_block4_1(x)   #common branch
         # x_2 = self.conv_block4_2(x, self.pool_type, pool_size=(2, 2))   #task specific branch
         #x: (#bs, #filt, #mel, #seq)
 
