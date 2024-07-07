2d3013f0fd167cd8c8b98aaec42298cbe8a6d2e3
Alexander Ivkin <a@ivkin.dev>
2024-07-07 17:05:10 +0200

    scripts

--- copy_blacklisted.py
+++ copy_blacklisted.py
@@ -5,20 +5,12 @@ import sys
 def get_basename_without_extension(file):
     return os.path.splitext(os.path.basename(file))[0]
 
-def copy_files_without_blacklist(source_dir, target_dir, blacklist_dir):
-    # Get a set of basenames (without extensions) from the blacklist directory
-    blacklist_files = set(
-        get_basename_without_extension(f) for f in os.listdir(blacklist_dir) 
-        if os.path.isfile(os.path.join(blacklist_dir, f))
-    )
-
-    # Loop through all files in the source directory
+def copy_files_without_blacklist(source_dir, target_dir, blacklist_files):
     for file in os.listdir(source_dir):
         source_file_path = os.path.join(source_dir, file)
         if os.path.isfile(source_file_path):
             base_filename = get_basename_without_extension(file)
 
-            # If the filename is not in the blacklist, copy it to the target directory
             if base_filename not in blacklist_files:
                 shutil.copy(source_file_path, target_dir)
                 print(f"Copied {file} to {target_dir}")
@@ -28,10 +20,24 @@ def copy_files_without_blacklist(source_dir, target_dir, blacklist_dir):
 if __name__ == "__main__":
     source_dir = sys.argv[1] 
     target_dir = sys.argv[2] 
-    blacklist_dir = sys.argv[3]
+    blacklist = sys.argv[3]
 
     # Ensure target directory exists
     os.makedirs(target_dir, exist_ok=True)
+    
+    if os.path.isdir(blacklist):
+        # Get a set of basenames (without extensions) from the blacklist directory
+        blacklist_files = set(
+            get_basename_without_extension(f) for f in os.listdir(blacklist) 
+            if os.path.isfile(os.path.join(blacklist, f))
+        )
+    else: # csv file
+        with open(blacklist) as f:
+            blacklist_files = set(
+                get_basename_without_extension(line.strip()) for line in f
+            )
+            
+    print(f"Blacklist files: {blacklist_files}")
 
     # Call the function to copy files
-    copy_files_without_blacklist(source_dir, target_dir, blacklist_dir)
+    copy_files_without_blacklist(source_dir, target_dir, blacklist_files)
--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ cd /acetrace/git
 
 aws configure
 aws s3 cp s3://acetrace/wav_2024_06_29/ /acetrace/git/data/wav --recursive
+aws s3 cp s3://acetrace/wav_2024_07_06/ /acetrace/git/data/wav_07_06 --recursive
 
 mkdir /acetrace/git/data/Tau_sound_events_2019/
 mkdir /acetrace/git/data/Tau_sound_events_2019/raw
@@ -12,3 +13,5 @@ mkdir /acetrace/git/data/Tau_sound_events_2019/raw/metadata_eval
 
 cp /acetrace/git/data/wav/*.wav /acetrace/git/data/Tau_sound_events_2019/raw/foa_eval/
 cp /acetrace/git/data/wav/*.csv /acetrace/git/data/Tau_sound_events_2019/raw/metadata_eval/
+cp /acetrace/git/data/wav_07_06/*.wav /acetrace/git/data/Tau_sound_events_2019/raw/foa_eval/
+cp /acetrace/git/data/wav_07_06/*.csv /acetrace/git/data/Tau_sound_events_2019/raw/metadata_eval/
